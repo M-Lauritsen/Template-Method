730243deb0d44c335d96faa6502bf8bc7b16fe9e
Michael Lauritsen <i30288741@live.dk>
2018-10-16 14:51:56 +0200

    Half way

--- Template Method/Program.cs
+++ Template Method/Program.cs
@@ -11,5 +11,36 @@ namespace Template_Method
         static void Main(string[] args)
         {
         }
+        abstract class Bread
+        {
+            public abstract void MixIngredients();
+
+            public abstract void Bake();
+
+            public virtual void Slice()
+            {
+                Console.WriteLine("Slicing the " + GetType().Name + " bread!");
+            }
+
+            public void Make()
+            {
+                MixIngredients();
+                Bake();
+                Slice();
+            }
+        }
+
+        class TwelveGrain : Bread
+        {
+            public override void MixIngredients()
+            {
+                Console.WriteLine("Gathering ingredients for 12-Grain Bread.");
+            }
+
+            public override void Bake()
+            {
+                Console.WriteLine("Baking the 12-Grain Bread. (25 minutes)");
+            }
+        }
     }
 }
