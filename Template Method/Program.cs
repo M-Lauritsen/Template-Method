79a8c4295a668d5e6693a01166e62ffa9bc0a828
Michael Lauritsen <i30288741@live.dk>
2018-10-17 07:41:49 +0200

    Template Method Done

--- Template Method/Program.cs
+++ Template Method/Program.cs
@@ -8,9 +8,7 @@ namespace Template_Method
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-        }
+
         abstract class Bread
         {
             public abstract void MixIngredients();
@@ -41,6 +39,46 @@ namespace Template_Method
             {
                 Console.WriteLine("Baking the 12-Grain Bread. (25 minutes)");
             }
+        }
+
+        class Sourdough : Bread
+        {
+            public override void MixIngredients()
+            {
+                Console.WriteLine("Gatherin Ingredients for Sourdough Bread");
+            }
+
+            public override void Bake()
+            {
+                Console.WriteLine("Baking the Sourdough Bread. (20 minutes)");
+            }
+        }
+
+        class WholeWheat : Bread
+        {
+            public override void MixIngredients()
+            {
+                Console.WriteLine("Gathering Ingredients for Sourdough Bread.");
+            }
+
+            public override void Bake()
+            {
+                Console.WriteLine("Baking the Whole Wheat Bread. (15 minutes)");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Sourdough sourdough = new Sourdough();
+            sourdough.Make();
+
+            TwelveGrain twelveGrain = new TwelveGrain();
+            twelveGrain.Make();
+
+            WholeWheat wholeWheat = new WholeWheat();
+            wholeWheat.Make();
+
+            Console.ReadKey();
         }
     }
 }
